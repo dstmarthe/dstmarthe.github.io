ca110a3110dcb9022e752ba2aff5ac24f3f0f920
Dale <72896959+dstmarthe@users.noreply.github.com>
2021-01-09 02:30:41 -0500

    Added mobile repo dropdown functionality

--- bScript.js
+++ bScript.js
@@ -1,6 +1,7 @@
-
 const domContainer = document.querySelector("#projInfo");
-var linkList = document.querySelectorAll(".project-tile");
+const linkList = document.querySelectorAll(".project-tile");
+var infoPara = document.createElement("p")
+infoPara.id = "mblInfo";
 const searchQueryURL = "https://api.github.com/users/dstmarthe/repos";
 domContainer.innerHTML =
 	"Mouse over a project to see a description, then click to visit each site.";
@@ -13,27 +14,66 @@ function hover(element, enter, leave) {
 	element.forEach((link) => link.addEventListener("mouseenter", enter));
 	element.forEach((link) => link.addEventListener("mouseleave", leave));
 }
-//added click function skeleton
-// function mobileClick(element, toggle_visibility(this)) {
-// 	element.forEach((link) => link.addEventListener("click", enter));
-// }
+//node takes number from function call in html
+var node;
+function changeNode(num) {
+	node=+num;
+}
+//Toggle repo information for mobile
+var v = function toggle_visibility() {
+	//If there is more than one p child delete inforPara, else add it
+	if (this.querySelectorAll("p").length >= 1){
+	infoPara.remove();
+	} else {
+	this.append(infoPara)}
+	getRepo(node, infoPara)
+}
 
-// function toggle_visibility(el) {
-// 	infoPara = document.createElement("p")
-// 	infoPara.id = "mblInfo"
-// 	if (this){}
+//Add event listeners to the project tiles, triggering toggle_visibility
+function mobileClick(el)  {
+	el.forEach((link) => link.addEventListener("click", v, false));
+}
+//Will be invoked to remove listeners on desktop
+function removeClick(el) {
+	el.forEach((link) => link.removeEventListener('click', v, false));	
+}
 
-// 	else {
-// 	el.insertAdjacentElement("afterend",infoPara)}
-// }
+function removeHover(element, enter, leave) {
+	element.forEach((link) => link.removeEventListener("mouseenter", enter));
+	element.forEach((link) => link.removeEventListener("mouseleave", leave));
+}
 
-async function getRepo(num) {
+function mediaQ(x) {
+	if (x.matches) { // If media query matches attach mobile function
+		mobileClick(linkList)
+		removeHover(linkList)
+	} else {
+		removeClick(linkList)
+		hover(                //Invoke hover for desktop
+			linkList,
+			(e) => {
+				// On hover
+				getRepo(node, domContainer);
+			},
+			(e) => {
+				// On exit hover
+				domContainer.innerHTML = "";
+			}
+		);
+	}
+  }
+  //Keeps track of window size
+  var x = window.matchMedia("(max-width: 760px)")
+  mediaQ(x) // Call listener function at run time
+  x.addEventListener("change", mediaQ) // Attach listener function on state changes
+
+async function getRepo(num, container) {
 	//Get repo info by index
 	fetch(searchQueryURL)
 		.then((result) => result.json())
 		.then((response) => {
 			//Returm name and description of repo by index
-			domContainer.innerHTML = `<em>Name:</em> ${response[num].name.replace(
+			container.innerHTML = `<em>Name:</em> ${response[num].name.replace(
 				"-",
 				" "
 			)}
@@ -42,23 +82,6 @@ async function getRepo(num) {
 		.catch((err) => console.log(err));
 }
 
-//node takes number from function call in html
-var node;
-function changeNode(num) {
-	node=+num;
-}
-
-hover(
-	linkList,
-	(e) => {
-		// On hover
-		getRepo(node);
-	},
-	(e) => {
-		// On exit hover
-		domContainer.innerHTML = "";
-	}
-);
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Get all "navbar-burger" elements
